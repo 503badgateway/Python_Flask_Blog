723b0a25477b90b26ab1c19d54fd065c15ff494a
homanlokqwq <e221042@ms.fkyc.edu.hk>
2023-12-11 16:11:13 +0800

    Feed and WIP

--- index.py
+++ index.py
@@ -26,6 +26,18 @@ except:
     print("Warning: 404 file not exists")
     nf_File = str(" ")
 
+#Loading config.json (not working for now)
+try:
+    f2 = open(f"./config/config.json", "r", encoding="utf-8").read()
+    y2 = json.loads(f2)
+    Url = f2["Url"]
+    Title = f2["Title"]
+    Description = f2["Description"]
+
+except:
+    print("Warning: config flie not exists")
+    Url = Title = Description = ""
+
 
 
 @app.route("/")
@@ -38,14 +50,27 @@ def Home_Page():
             try:
                 f = open(f"./posts/" + arr1, "r", encoding="utf-8").read()
                 y = json.loads(f.split("---")[0])
-                title1 = y["title"]
-                des = y["description"]
-                print(title1 + des)
+                try:
+                    title1 = y["title"]
+                except:
+                    title1 = "This Post dont have a title"
+                try:
+                    des = y["description"]
+                except:
+                    des = "This Post dont have a description"
+                try:
+                    tag = ""
+                    for tags in y["tags"]:
+                        tag += "," + tags
+                except:
+                    tag = str("None")
+                print(title1 + des + tag)
             except:
                 title1 = "This Post dont have a title"
                 des = "This Post dont have a description"
+                tag = str("None")
             Link = "./posts/" + arr1.split(".")[0]
-            return_File += render_template('posts.html', title=title1, description=des, href=Link)
+            return_File += render_template('posts.html', tag=tag, title=title1, description=des, href=Link)
         try:
             return header_File + return_File + footer_File
         except Exception as e:
@@ -74,3 +99,23 @@ def show_post(post_id):
 @app.route("/feed")
 def Feed():
     return Load()
+
+@app.route("/links")
+def links():
+    return_File = ""
+    try:
+        f1 = open(f"./config/links.json", "r", encoding="utf-8").read()
+        y1 = json.loads(f1)
+        try:
+            for y1 in y1:
+                Link_title = y1["Link_title"]
+                Link_des = y1["Link_des"]
+                Link_url = y1["Link_url"]
+                return_File += render_template('links.html', Link_title=Link_title, Link_des=Link_des, Link_url=Link_url)
+        except:
+            pass
+        return header_File + return_File + footer_File
+    except Exception as e:
+        print(f"An exception occurred: {str(e)}")
+        return_Content = header_File + nf_File + footer_File
+        return return_Content, 500
